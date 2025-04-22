ec261a658c49d8df621a525337f32cc960f6fb7f
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2025-04-22 22:56:54 +0300

    send telegram

--- send_telegram.sh
+++ send_telegram.sh
@@ -6,21 +6,24 @@ WEBHOOK_URL="https://api.telegram.org/bot6796421307:AAHmQ9jvbJl9kslUnPR5W5beV5EC
 CHAT_ID="439653349"
 COMPOSE_PATH="/home/prod_server/production/monitoring/docker-compose.yaml"
 
-# Выполняем обновление
-OUTPUT=$(certbot renew --quiet --deploy-hook "docker compose -f $COMPOSE_PATH restart nginx" 2>&1)
+
+# Выполняем обновление и сохраняем вывод
+OUTPUT=$(certbot renew --deploy-hook "docker compose -f $COMPOSE_PATH restart nginx" 2>&1)
 EXIT_CODE=$?
 
-# Проверка результата
-if [[ $EXIT_CODE -eq 0 ]]; then
-    if echo "$OUTPUT" | grep -q "No renewals were attempted"; then
-        MESSAGE="ℹ️ Сертификаты не требуют обновления. Всё в порядке."
-    else
-        MESSAGE="✅ Сертификат(ы) были обновлены и nginx перезапущен."
-    fi
-else
+# Проверка по содержимому вывода
+if [[ $EXIT_CODE -ne 0 ]]; then
     MESSAGE="❌ Ошибка при обновлении сертификатов:\n$OUTPUT"
+elif echo "$OUTPUT" | grep -q "No renewals were attempted"; then
+    MESSAGE="ℹ️ Сертификаты не требуют обновления. Всё в порядке."
+elif echo "$OUTPUT" | grep -q "Congratulations, all renewals succeeded"; then
+    MESSAGE="✅ Сертификат(ы) были обновлены и nginx перезапущен."
+else
+    MESSAGE="⚠️ Неизвестный результат обновления:\n$OUTPUT"
 fi
 
 # Отправка сообщения
-curl -s -X POST "$WEBHOOK_URL" -d chat_id="$CHAT_ID" -d text="$MESSAGE" -d parse_mode="Markdown"
-
+curl -s -X POST "$WEBHOOK_URL" \
+  -d chat_id="$CHAT_ID" \
+  -d text="$MESSAGE" \
+  -d parse_mode="Markdown"
