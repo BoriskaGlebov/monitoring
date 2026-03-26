7991516c642ab6fc47c6a6a529cdd3ff9fd4ba3f
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2026-03-26 22:46:52 +0300

    sock5 proxy

--- socks5_proxy/entrypoint.sh
+++ socks5_proxy/entrypoint.sh
@@ -13,8 +13,6 @@ log /usr/local/3proxy/logs/3proxy.log
 logformat "-\""+_G{""time_unix"":%t, ""proxy"":{""type:"":""%N"", ""port"":%p}, ""error"":{""code"":""%E""}, ""auth"":{""user"":""%U""}, ""client"":{""ip"":""%C"", ""port"":%c}, ""server"":{""ip"":""%R"", ""port"":%r}, ""bytes"":{""sent"":%O, ""received"":%I}, ""request"":{""hostname"":""%n""}, ""message"":""%T""}"
 auth strong
 allow $SOCKS5_LOGIN
-deny *
-
 socks -p$SOCKS5_PORT
 EOF
 
