b89139f8673c52b6ddfaa88117978f6513ff9c70
BoriskaGlebov <boristheblade.glebov@yandex.ru>
2026-03-27 11:32:26 +0300

    sock5 proxy 8443

--- socks5_proxy/entrypoint.sh
+++ socks5_proxy/entrypoint.sh
@@ -2,17 +2,26 @@
 
 echo "Starting 3proxy..."
 
+# Создаём файл пользователей
+cat <<EOF > /usr/local/3proxy/conf/users.txt
+$SOCKS5_LOGIN:CL:$SOCKS5_PASSWORD
+EOF
+
+# Создаём конфиг
 cat <<EOF > /usr/local/3proxy/conf/3proxy.cfg
 #!/bin/3proxy
 
 timeouts 1 5 30 60 180 1800 15 60
 
-users $SOCKS5_LOGIN:CL:$SOCKS5_PASSWORD
+users \$/usr/local/3proxy/conf/users.txt
+
 log /usr/local/3proxy/logs/3proxy.log
 logformat "-\""+_G{""time_unix"":%t, ""proxy"":{""type:"":""%N"", ""port"":%p}, ""error"":{""code"":""%E""}, ""auth"":{""user"":""%U""}, ""client"":{""ip"":""%C"", ""port"":%c}, ""server"":{""ip"":""%R"", ""port"":%r}, ""bytes"":{""sent"":%O, ""received"":%I}, ""request"":{""hostname"":""%n""}, ""message"":""%T""}"
+
 auth strong
 socks -p$SOCKS5_PORT -i0.0.0.0 -e0.0.0.0
 EOF
+
 echo "Container startup"
 
 /bin/3proxy /usr/local/3proxy/conf/3proxy.cfg
